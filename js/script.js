e9e8b1cbe96705f62a5b85a09e41d3c9db64aa30
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-04-25 19:00:11 +1000

    statements added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-let getEl = document.querySelector('.banner h1');
+const oddLinks = document.querySelectorAll('#navbar li:nth-child(odd)');
+console.log(oddLinks);
+
+oddLinks.forEach(function(odd) {
+    odd.style.backgroundColor = 'red';
+    odd.style.color  = 'white';
+});
