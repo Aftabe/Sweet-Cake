980d51c7f69f9448d8c6a2097c4c06967ae7be86
Aftabe <shamsasghari77@gmail.com>
2020-08-24 20:54:04 +1000

    new testing

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 (function(){
     let name,
-    medium,
     outLine;
-    name = prompt('What\s your name?');
+    name = prompt('Please type your name');
     if(name === '' || name === '0' || name === null || name > 0){
         window.location.reload();
-    } else if(name === 'Ali' || name === 'Haje' || name === 'Khan'){
-      medium = 'Welcome dear';  
-    }else{
-        outLine = 'Welcome dear';
+    } else{
+        outLine = alert('Welcome dears');
     }
 }());
