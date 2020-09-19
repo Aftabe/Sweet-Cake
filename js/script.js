72b62f3caf5beefd7b5f2cdea05a62d2dac3929c
Aftabe <shamsasghari77@gmail.com>
2020-09-19 20:42:44 +1000

    some js comde added

--- js/script.js
+++ js/script.js
@@ -1,25 +1,14 @@
-(function(){
-    let name,
+(function () {
+  let name,
     outLine;
-    name = prompt('Please type your name');
-    if(name === '' || name === '0' || name === null || name > 0){
-        window.location.reload();
-    } else{
-        outLine = alert('Welcome dears');
-    }
+  name = prompt('Please type your name');
+  if (name === '' || name === '0' || name === null || name > 0) {
+    window.location.reload();
+  }
 }());
 
 
-$(document).ready(function(){
-    $(".jQuery-btn").click(function(){
-      $(".jQuery-text").css("color", "green").slideUp(2000).slideDown(2000);
-    });
-  });
 
-
-
-  let doc = document.getElementsByTagName('a');
-
-doc.addEventListners('click', functionDo(e){
-e.preventDefault();
-})
+$(".jQuery-btn").click(function () {
+  $(".jQuery-text").css("color", "green").slideUp(2000).slideDown(2000);
+});
