0cda79e8708891f474fa7df5b726910152c37fb7
Aftabe <shamsasghari77@gmail.com>
2020-10-14 21:10:15 +1100

    for more accuracy

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 (function () {
   let name;
   name = prompt('Please type your name');
-  if (name === '' || name === '0' || name > 0 || name === null) {
+  if (name === '' || name === '0' || name.length < 3|| name > 0 || name === null) {
     window.location.reload();
     alert('Please type your name');
   }
