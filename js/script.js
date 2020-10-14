efe721a852d3ccfa2c4d6f05f1f110abc61ee3c5
Aftabe <shamsasghari77@gmail.com>
2020-10-14 21:01:32 +1100

    come js code just removed

--- js/script.js
+++ js/script.js
@@ -3,12 +3,6 @@
   name = prompt('Please type your name');
   if (name === '' || name === '0' || name > 0 || name === null) {
     window.location.reload();
-    alert('You should type your name in the field');
+    alert('Please type your name');
   }
 }());
-
-
-
-$(".jQuery-btn").click(function () {
-  $(".jQuery-text").css("color", "green").slideUp(2000).slideDown(2000);
-});
